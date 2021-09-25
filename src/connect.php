5b6edb89124b41f70992c47f59c39cb17cfa4d4e
DIDRICH Damien <89045920+skunkoff1@users.noreply.github.com>
2021-09-25 11:03:51 +0200

    Update connect.php

--- src/connect.php
+++ src/connect.php
@@ -1,3 +1,3 @@
 <?php
 
-$db = new PDO('mysql: host=localhost:3306; dbname=hostimage; charset=utf8', 'root', 'mdp');
+$db = new PDO('mysql: host=localhost:3306; dbname=hostimagedb; charset=utf8', 'root', 'mdp');
